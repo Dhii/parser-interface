d7a9aaeda0b062b7e6d636c8b725e1b85068e704
Anton Ukhanev <xedin.unknown@gmail.com>
2017-08-27 20:27:49 +0200

    Documentation for `ParserInterface` made more verbose

--- src/ParserInterface.php
+++ src/ParserInterface.php
@@ -11,6 +11,19 @@ use Dhii\Io\Resource\ReadableResourceInterface;
  * syntax tree. The more obvious examples include XML and JSON. Less obvious
  * examples include SQL, structured English, or even a `printf()` format string.
  *
+ * It isn't always effective to have the whole string in order to start parsing.
+ * For example, the parser may want to return information about only some
+ * of the nodes represented in the source; in this case, holding the whole
+ * source string in memory can be undesirable. If only some information about
+ * the nodes should be retrieved, the problem becomes more severe, especially
+ * if only a certain maximal amount of nodes is required in the output; in this
+ * case, a very large string in memory must traditionally be parsed into a DOM
+ * first, before the desired data can be retrieved from it. This parser solves
+ * that problem by moving through the resource in a linear fashion, perhaps
+ * reading only a small amount of data into memory at a time, and/or decoding
+ * only some of the nodes in the resource, perhaps conditionally, or only
+ * attaching some information to them.
+ *
  * @since [*next-version*]
  */
 interface ParserInterface
