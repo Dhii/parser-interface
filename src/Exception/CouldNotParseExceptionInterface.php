d571646076183d557458380c62db6a7db59edf62
Anton Ukhanev <xedin.unknown@gmail.com>
2017-08-27 20:31:31 +0200

    Added `CouldNotParseExceptionInterface`

---
+++ src/Exception/CouldNotParseExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Parser\Exception;
+
+/**
+ * An exception that occurs when a parser cannot parse.
+ *
+ * @since [*next-version*]
+ */
+interface CouldNotParseExceptionInterface extends ParserExceptionInterface
+{
+}
